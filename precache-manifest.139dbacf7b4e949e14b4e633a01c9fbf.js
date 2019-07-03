5ff5b62cae8cc110807f5a28028f155c6dc51c95
gagarinbefree <gagarinbefree@gmail.com>
2019-07-03 14:28:02 +0500

    Updates

---
+++ precache-manifest.139dbacf7b4e949e14b4e633a01c9fbf.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "30f0842efc4dcfac4b4dcf3f42e16ed5",
+    "url": "/webim/index.html"
+  },
+  {
+    "revision": "fe37c9bd12a671b83d82",
+    "url": "/webim/static/css/main.2cce8147.chunk.css"
+  },
+  {
+    "revision": "26d2b8f580d08b498f3f",
+    "url": "/webim/static/js/2.dec4c6c2.chunk.js"
+  },
+  {
+    "revision": "fe37c9bd12a671b83d82",
+    "url": "/webim/static/js/main.4c180bf2.chunk.js"
+  },
+  {
+    "revision": "58257d2a41573cba9fe5",
+    "url": "/webim/static/js/runtime~main.5aca072b.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/webim/static/media/logo.5d5d9eef.svg"
+  }
+]);
