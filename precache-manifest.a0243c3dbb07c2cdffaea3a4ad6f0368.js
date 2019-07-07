7d9964dcd5d0fc5f372e148b1d0780d4e2ba6b1c
gagarinbefree <gagarinbefree@gmail.com>
2019-07-07 17:37:35 +0500

    Updates

---
+++ precache-manifest.a0243c3dbb07c2cdffaea3a4ad6f0368.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8883b11a1f985f5b0701c5d04c8c9645",
+    "url": "/webim/index.html"
+  },
+  {
+    "revision": "f393abbc5272f67c2870",
+    "url": "/webim/static/css/2.510409cd.chunk.css"
+  },
+  {
+    "revision": "4225a0d170fa4b08d4ed",
+    "url": "/webim/static/css/main.c61c99ec.chunk.css"
+  },
+  {
+    "revision": "f393abbc5272f67c2870",
+    "url": "/webim/static/js/2.abd0d98f.chunk.js"
+  },
+  {
+    "revision": "4225a0d170fa4b08d4ed",
+    "url": "/webim/static/js/main.a8ba169b.chunk.js"
+  },
+  {
+    "revision": "58257d2a41573cba9fe5",
+    "url": "/webim/static/js/runtime~main.5aca072b.js"
+  }
+]);
